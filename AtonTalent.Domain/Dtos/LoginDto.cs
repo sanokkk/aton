1cdd81c34b9e446936335d4b46ce8d4fcb2426e2
sanokkk <sashaevstr2002@yandex.ru>
2023-04-17 18:32:10 +0300

    added loginto for access checking

---
+++ AtonTalent.Domain/Dtos/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtonTalent.Domain.Dtos
+{
+    public class LoginDto
+    {
+        public string Login { get; init; }
+
+        public string Password { get; init; }
+    }
+}
