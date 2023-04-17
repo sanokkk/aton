a2e8986afe61abaf1887a9019831f6e74405cb40
sanokkk <sashaevstr2002@yandex.ru>
2023-04-17 18:18:56 +0300

    added dto model and mapper

---
+++ AtonTalent.DAL/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AtonTalent.Domain.Dtos;
+using AtonTalent.Domain.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtonTalent.DAL.Profiles
+{
+     class UserProfile: Profile
+     {
+        public UserProfile() 
+        {
+            CreateMap<User, UserCreateDto>();
+            CreateMap<UserCreateDto, User>();
+        }
+     }
+}
