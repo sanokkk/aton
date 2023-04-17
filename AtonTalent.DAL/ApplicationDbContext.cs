36aea1fabe7e7b00a30273a3f113ca6c74de1180
sanokkk <sashaevstr2002@yandex.ru>
2023-04-17 11:35:31 +0300

    added default user

--- AtonTalent.DAL/ApplicationDbContext.cs
+++ AtonTalent.DAL/ApplicationDbContext.cs
@@ -17,5 +17,37 @@ namespace AtonTalent.DAL
         }
 
         public DbSet<User> Users;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = Guid.NewGuid(),
+                Login = "sanokkk",
+                Password = "password",
+                Name = "Alexander",
+                Gender = 1,
+                Birtday = new DateTime(year: 2002, month: 9, day: 18),
+                Admin = true,
+                CreatedOn = DateTime.Now,
+                CreatedBy = "sanokkk",
+            });
+            modelBuilder.Entity<User>().HasKey(x => x.Id);
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<User>().Property(p => p.Id).HasColumnName(nameof(User.Id).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.Login).HasColumnName(nameof(User.Login).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.Password).HasColumnName(nameof(User.Password).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.Name).HasColumnName(nameof(User.Name).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.Gender).HasColumnName(nameof(User.Gender).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.Birtday).HasColumnName(nameof(User.Birtday).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.Admin).HasColumnName(nameof(User.Admin).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.CreatedOn).HasColumnName(nameof(User.CreatedOn).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.CreatedBy).HasColumnName(nameof(User.CreatedBy).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.ModifiedOn).HasColumnName(nameof(User.ModifiedOn).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.ModifiedBy).HasColumnName(nameof(User.ModifiedBy).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.RevokedOn).HasColumnName(nameof(User.RevokedOn).ToLower());
+            modelBuilder.Entity<User>().Property(p => p.RevokedBy).HasColumnName(nameof(User.RevokedBy).ToLower());
+        }
     }
 }
