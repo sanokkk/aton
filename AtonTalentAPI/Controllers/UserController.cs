37b5b39b3aaf2e06260e8bcbf3315731cc70fc6d
sanokkk <sashaevstr2002@yandex.ru>
2023-04-17 18:07:26 +0300

    added controller

---
+++ AtonTalentAPI/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtonTalentAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserController: ControllerBase
+    {
+
+    }
+}
